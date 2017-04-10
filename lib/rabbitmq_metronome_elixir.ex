984054bfc2e98d995305cf9e5f69dfb523ba2d31
Alexey Lebedeff <alebedev@mirantis.com>
2017-04-10 21:01:20 +0300

    `mix new` boilerplate

---
+++ lib/rabbitmq_metronome_elixir.ex
@@ -0,0 +1,18 @@
+defmodule RabbitmqMetronomeElixir do
+  @moduledoc """
+  Documentation for RabbitmqMetronomeElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RabbitmqMetronomeElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rabbitmq_metronome_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule RabbitmqMetronomeElixirTest do
+  use ExUnit.Case
+  doctest RabbitmqMetronomeElixir
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
